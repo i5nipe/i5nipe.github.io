43efddc13ca47256ba945b91914afebb86fab4cc
i5nipe <thalles.isnipe@gmail.com>
2025-01-02 20:18:30 -0300

    Workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://blog.i5nipe.com/",
   base: "/",
   trailingSlash: "always",
   integrations: [
