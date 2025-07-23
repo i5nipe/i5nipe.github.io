0ab955f77078549f508801c15b6b20d58bfe7b93
i5nipe <thalles.isnipe@gmail.com>
2025-07-22 22:16:37 -0300

    making google search happy

--- src/config.ts
+++ src/config.ts
@@ -34,7 +34,8 @@ export const siteConfig: SiteConfig = {
 	},
 	favicon: [
 		// Leave this array empty to use the default favicon
-		{ src: "/favicon/favicon.ico", theme: "light", sizes: "32x32" },
+		{ src: "/favicon.ico", //theme: "light", sizes: "32x32" 
+    },
 		// {
 		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
 		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
